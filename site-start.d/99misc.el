c1cbf7d477bf7c2de37f77fbab2182298f7406fc
Junya KOBORI <junya@mewtone.jp>
2015-04-04 01:28:26 +0900

    編集日時自動挿入のlispを最期にもってきた

--- site-start.d/99misc.el
+++ site-start.d/99misc.el
@@ -4,17 +4,6 @@
 ;; dired利用時、ディレクトリを先頭に並べる
 (setq ls-lisp-dirs-first t)
 
-;; 最終変更日の自動挿入
-;; ファイルの先頭から8行以内に Revised: <Thu Apr 02 10:17:35 2015 junya> または
-;; Revised: " " と書いてあれば、セーブ時に自動的に日付が挿入される
-(require 'time-stamp)
-(add-hook 'write-file-hooks 'time-stamp)
-(setq time-stamp-active t)
-(setq time-stamp-start "Revised: <")
-(setq time-stamp-format "%3a %3b %02d %02H:%02M:%02S %:y %u")
-(setq time-stamp-end ">")
-(setq time-stamp-line-limit 12) ;; 変ていいのかな? time-stamp.elには変えるなと書いてある
-
 ;; mode lineに時間表示
 (setq display-time-24hr-format t)
 (setq display-time-string-forms '(month "/" day " " 24-hours ":" minutes))
@@ -39,3 +28,14 @@
   (setq ls-lisp-use-insert-directory-program nil));;
 
 (add-hook 'dired-load-hook (lambda () (load "dired-x")))
+
+;; 最終変更日の自動挿入
+;; ファイルの先頭から8行以内に Revised: <> または
+;; Revised: " " と書いてあれば、セーブ時に自動的に日付が挿入される
+(require 'time-stamp)
+(add-hook 'write-file-hooks 'time-stamp)
+(setq time-stamp-active t)
+(setq time-stamp-start "Revised: <")
+(setq time-stamp-format "%3a %3b %02d %02H:%02M:%02S %:y %u")
+(setq time-stamp-end ">")
+(setq time-stamp-line-limit 12) ;; 変ていいのかな? time-stamp.elには変えるなと書いてある
