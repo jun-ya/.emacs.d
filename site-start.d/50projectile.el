866630cc42984ff144f4c6cfcb2b79b368178639
Junya KOBORI <junya@mewtone.jp>
2017-02-01 20:08:02 +0900

    helmの設定をコメント化

--- site-start.d/50projectile.el
+++ site-start.d/50projectile.el
@@ -1,5 +1,5 @@
 ;; 参考: http://tech.camph.net/projectile-and-helm/
 ;; keybind: https://github.com/bbatsov/projectile
 (projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
+;;(setq projectile-completion-system 'helm)
+;;(helm-projectile-on)
